a2f03ca7b32e8e904a677ccca93bc6c7f573b3bd
alxmart <alxmartinez@gmail.com>
2023-02-06 20:11:14 -0300

    Project created.

---
+++ app/src/main/java/com/luizafmartinez/modulo4_java/Testes.java
@@ -0,0 +1,35 @@
+package com.luizafmartinez.modulo4_java;
+
+public class Testes {
+}
+
+//
+//class Scratch {
+//
+//    public static void main(String[] args) {
+//
+////        String nome = "Homer J. Simpson";
+////        Integer idade = 35;
+////        idade = 20;
+////        System.out.println("Usuário: " + nome + "\nIdade: " + idade);
+////        final Double PI = 3.14;
+////        final Integer QUANTIDADE_PESSOAS_COMPRA = 1000;
+////        System.out.println(PI);
+//
+//        byte idade = 52;
+//        System.out.println(idade);
+//        int numeroCasa = 360;
+//        float preco = 34.90f;
+//        double total = 456.9867;
+//        boolean visivel = true;
+//        System.out.println("resultado: " + visivel);
+//
+//        System.out.println("Idade: " + idade);
+//
+//        // Classe Wrapper
+//        Byte idadeC = 52;
+//        System.out.println(idadeC);
+//        char carac = 'F';
+//        System.out.println(carac);
+//    }
+//}
