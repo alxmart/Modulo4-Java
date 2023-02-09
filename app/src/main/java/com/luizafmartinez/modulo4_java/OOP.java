4220d6d19e3cc707ababefde05c29f03c6b83052
alxmart <alxmartinez@gmail.com>
2023-02-09 16:28:05 -0300

    Adding tests and exercises.

---
+++ app/src/main/java/com/luizafmartinez/modulo4_java/OOP.java
@@ -0,0 +1,85 @@
+package com.luizafmartinez.modulo4_java;
+
+/*
+
+1) Programação Linear
+======================
+
+permite ao usuário digitar numero 1
+permite ao usuário digitar numero 2
+
+somar numero 1 com numero 2
+
+exibir o resultado da soma
+
+2) Programação Estruturada
+============================
+
+funcao somar
+    permite ao usuário digitar numero 1
+    permite ao usuário digitar numero 2
+
+    somar numero 1 com numero 2
+    exibir o resultado da soma
+
+processo 01
+funcao somar
+processo 02
+funcao somar
+
+3) Programação Modular
+=======================
+
+Módulo Usuario
+    funcao logar
+        recebe e-mail e senha
+        verifica se o usário existe
+        envia esse usuário a tela principal
+
+    funcao deslogar
+        sair do sistema
+        vai para tela inicial
+
+** Tela inicial do sistema
+Botão -> logar
+
+** Tela principal do sistema
+Botao -> deslogar
+
+
+** tela de fornecedores
+deslogar
+logar
+
+3) Programação Orientada a objetos
+Classe fornecedor, cliente, produto...
+Classe Usuario
+    funcao logar
+        recebe e-mail e senha
+        verifica se o usário existe
+        envia esse usuário a tela principal
+
+    funcao deslogar
+        sair do sistema
+        vai para tela inicial
+
+Lower Camel Case
+jogoMarioKart
+
+Upper Camel Case
+JogoMarioKart
+
+Snake Case
+jogo_mario_kart
+
+Kebabe Case
+jogo-mario-kart
+
+
+*/
+
+class Scratch1 {
+    public static void main(String[] args) {
+
+    }
+}
