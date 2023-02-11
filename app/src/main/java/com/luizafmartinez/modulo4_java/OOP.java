f2d3f66a5a323f50c993a54b0c2e562c06f13f47
alxmart <alxmartinez@gmail.com>
2023-02-11 17:41:22 -0300

    Adding tests and exercises.

--- app/src/main/java/com/luizafmartinez/modulo4_java/OOP.java
+++ app/src/main/java/com/luizafmartinez/modulo4_java/OOP.java
@@ -1,9 +1,7 @@
-package com.luizafmartinez.modulo4_java;
-
 /*
 
 1) Programação Linear
-======================
+=====================
 
 permite ao usuário digitar numero 1
 permite ao usuário digitar numero 2
@@ -12,8 +10,9 @@ somar numero 1 com numero 2
 
 exibir o resultado da soma
 
+
 2) Programação Estruturada
-============================
+===========================
 
 funcao somar
     permite ao usuário digitar numero 1
@@ -21,6 +20,8 @@ funcao somar
 
     somar numero 1 com numero 2
     exibir o resultado da soma
+
+------------------------------------------
 
 processo 01
 funcao somar
@@ -31,29 +32,38 @@ funcao somar
 =======================
 
 Módulo Usuario
+
     funcao logar
         recebe e-mail e senha
-        verifica se o usário existe
+        verifica se o usuário existe
         envia esse usuário a tela principal
 
     funcao deslogar
         sair do sistema
         vai para tela inicial
 
+--------------------------------------------------
+
 ** Tela inicial do sistema
+--------------------------
 Botão -> logar
 
 ** Tela principal do sistema
+----------------------------
 Botao -> deslogar
 
-
 ** tela de fornecedores
+----------------------------
 deslogar
 logar
 
-3) Programação Orientada a objetos
+3) Programação Orientada a Objetos
+===================================
+
 Classe fornecedor, cliente, produto...
+
 Classe Usuario
+
     funcao logar
         recebe e-mail e senha
         verifica se o usário existe
@@ -63,23 +73,49 @@ Classe Usuario
         sair do sistema
         vai para tela inicial
 
+===================================================================
+
 Lower Camel Case
+-----------------
 jogoMarioKart
 
 Upper Camel Case
+-----------------
 JogoMarioKart
 
 Snake Case
+-----------------
 jogo_mario_kart
 
 Kebabe Case
+-----------------
 jogo-mario-kart
 
-
+==========================================================================
 */
+class Jogador {
 
-class Scratch1 {
+    String kart;
+    String pneu;
+    String planador;
+
+    void acelerar() {
+        System.out.println("Acelerar: " + pneu );
+    }
+}
+
+class Scratch {
     public static void main(String[] args) {
 
+        Jogador homer = new Jogador();
+        homer.kart = "Normal";
+        homer.planador = "paraquedas";
+        homer.pneu = "cross";
+
+        Jogador computer = new Jogador();
+        computer.pneu = "Avançado";
+
+        homer.acelerar();
+        computer.acelerar();
     }
 }
